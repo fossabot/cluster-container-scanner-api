6c9f0fd997c90d6f6cbf5ed8a56407f9d9c64a17
Bezalel Brandwine <bbrandwine@armosec.io>
2021-12-07 17:21:43 +0200

    fix CalculateFixed name

--- containerscan/datastructures.go
+++ containerscan/datastructures.go
@@ -29,7 +29,7 @@ var KnownSeverities = map[string]bool{
 
 func CalculateFixed(Fixes []FixedIn) int {
 	for _, fix := range Fixes {
-		if fix.Version != "None" {
+		if fix.Version != "None" && fix.Name != "not-fixed" {
 			return 1
 		}
 	}
